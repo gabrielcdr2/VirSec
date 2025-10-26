91f984dc118e1d780ad0cd5b64f05f2f8940a477
Fabricio-Bandeira <fabriciobandeirafacul@gmail.com>
2025-10-26 17:24:11 -0300

    adicionado a pagina de listagem de alunos

--- alunos/urls.py
+++ alunos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AlunoCreateView 
+from .views import AlunoCreateView, AlunoListView
 
 app_name = 'alunos'
 
 urlpatterns = [
     path('cadastrar/', AlunoCreateView.as_view(), name='aluno_cadastrar'),
+    path('lista/', AlunoListView.as_view(), name='aluno_list'),
     # Adicionaremos outras URLs aqui no futuro (listar, editar, etc.)
 ]
--- alunos/views.py
+++ alunos/views.py
@@ -7,3 +7,14 @@ class AlunoCreateView(CreateView):
     fields = '__all__'
     template_name = 'alunos/cadastroAlunos.html'
     success_url = reverse_lazy('index')
+
+
+class AlunoListView(ListView):
+    model = Aluno
+    template_name = 'alunos/aluno_list.html'
+    context_object_name = 'alunos'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Lista de Alunos'
+        return context
