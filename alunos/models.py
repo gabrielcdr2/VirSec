2a591067fc39f4eaaab6d628817810f51c8c278d
Gabriel Campos <160979410+gabrielcdr2@users.noreply.github.com>
2025-10-26 15:24:00 -0300

    Estilizando o formulário de alunos

--- alunos/models.py
+++ alunos/models.py
@@ -5,6 +5,7 @@ class Aluno(models.Model):
     nome = models.CharField(max_length=100)
     idade = models.IntegerField()
     nascimento = models.DateField()
+    cpf = models.CharField(max_length=11, unique=True)
     data_cadastro = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
